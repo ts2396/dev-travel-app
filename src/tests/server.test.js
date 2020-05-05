ac7625ae2c3f7753917c0dcb4aace6878de208d7
ts2396 <48188551+ts2396@users.noreply.github.com>
2020-05-05 12:42:27 -0400

    FixedServerTest

--- src/tests/server.test.js
+++ src/tests/server.test.js
@@ -1,32 +1,21 @@
-import 'regenerator-runtime/runtime';
-import { appGetPictureURL, appGetWeatherURL } from '../server/index';
-
-const mockResponse = () => {
-  const res = {};
-  res.send = jest.fn().mockReturnValue(res);
-  return res;
-};
-
-const mockRequest = (sessionData) => {
-  return {
-    session: { data: sessionData },
-  };
-};
-
-test('should return right weatherAPIUrl', async () => {
-  const req = mockRequest();
-  const res = mockResponse();
-  const currentEnv = process.env;
-  process.env = { WB_API_ID: 'test', PB_API_ID: 'testPIX' };
-  await appGetWeatherURL(req, res);
-  expect(res.send).toHaveBeenCalledWith({ apiUrl: 'https://api.weatherbit.io/v2.0/forecast/test/?key=test'});
+const regeneratorRuntime = require("regenerator-runtime");
+test ("It should be a function", async() => {
+    expect(typeof getLatLng('Dallas Texas')).toBe("object");
 });
 
-test('should return right pictureAPIUrl', async () => {
-  const req = mockRequest();
-  const res = mockResponse();
-  const currentEnv = process.env;
-  process.env = { WB_API_ID: 'test', PB_API_ID: 'testPIX' };
-  await appGetPictureURL(req, res);
-  expect(res.send).toHaveBeenCalledWith({ apiUrl: 'https://pixabay.com/api/?key=testPIX'});
-});
+async function getLatLng (place){
+    const fetch = require('node-fetch');
+    const dotenv = require('dotenv');
+    dotenv.config();
+    const reqURL = 'http://api.geonames.org/searchJSON?q=';
+    const geoUsername = process.env.GN_ID;
+    const res = await fetch(`${reqURL}${place}&username=${geoUsername}`)
+    try {
+        const data = await res.json();
+        const lng = data.geonames[0].lng;
+        const lat = data.geonames[0].lat;
+        return[lng,lat];
+    } catch(error) {
+        console.log("error", error);
+    }
+}
